d3cafd18b934753d6059d9ad86744b524eeefcdb
matsumoto1199 <114039223+matsumoto1199@users.noreply.github.com>
2022-10-20 18:18:21 +0900

    プロジェクト ファイルを追加します。

---
+++ online1020/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace online1020.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ online1020/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace online1020.Models
+{
+    public partial class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+    }
+}
